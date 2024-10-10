2e3acca535bf1251f4750618eb9c84d957e7e635
max93776 <maxhdtube@gmail.com>
2024-10-10 13:34:36 +0200

    Minor changes

--- fm350gl.sh
+++ fm350gl.sh
@@ -6,14 +6,15 @@ TEMP_FILE=$(mktemp)
 LOG_FILE="/root/autostart/fm350gl_sh.log"
 START=$(date '+%Y-%m-%d %H:%M:%S')
 
+logger "Script fm350gl.sh started"
 echo "-----------------------------------------------------------" >> "$TEMP_FILE"
 echo "---$(date '+%Y-%m-%d %H:%M:%S')" >> "$TEMP_FILE"
 echo "0e8d 7127" > /sys/bus/usb-serial/drivers/generic/new_id
 echo "---Installed driver, now waiting 10s" >> "$TEMP_FILE"
-sleep 10
+sleep 10 
 
 for i in {1..10}; do
-	./autostart/at_commander at >> "$TEMP_FILE"
+	timeout 5s /root/autostart/at_commander at >> "$TEMP_FILE"
 	if tail -n 1 "$TEMP_FILE" | grep -q "OK";then
 		echo "---AT command accepted" >> "$TEMP_FILE"
 		break
@@ -22,7 +23,7 @@ for i in {1..10}; do
 done
 
 for i in  {1..5}; do
-	./autostart/at_commander at+cgdcont=1,\"IP\",\"internet\" >> "$TEMP_FILE"
+  timeout 5s /root/autostart/at_commander at+cgdcont=1,\"IP\",\"internet\" >> "$TEMP_FILE"
 	if tail -n 1 "$TEMP_FILE" | grep -q "OK";then
 		echo "---PDP context set" >> "$TEMP_FILE"
 		break
@@ -31,7 +32,7 @@ for i in  {1..5}; do
 done 
 
 for i in {1..5}; do 
-	./autostart/at_commander at+cgact=1,1 >> "$TEMP_FILE"
+	timeout 5s /root/autostart/at_commander at+cgact=1,1 >> "$TEMP_FILE"
 	if tail -n 1 "$TEMP_FILE" | grep -q "+CGEV";then
 		echo "---PDP activated" >> "$TEMP_FILE"
 		break
@@ -43,7 +44,7 @@ for i in {1..5}; do
 done 
 
 for i in {1..5]; do
-	./autostart/at_commander at+cgpaddr=1 >> "$TEMP_FILE"
+	timeout 5s /root/autostart/at_commander at+cgpaddr=1 >> "$TEMP_FILE"
 	if tail -n 1 "$TEMP_FILE" | grep -q "+CGPADDR:";then
 		echo "---IP address sent" >> "$TEMP_FILE"
 		break
@@ -57,15 +58,25 @@ echo "---IP address: $ip" >> "$TEMP_FILE"
 
 uci set network.FM350GL.ipaddr="$ip"
 uci commit network
-service network reload
+/etc/init.d/network reload
+
+echo "---Setting routes for $ip" >> "$TEMP_FILE"
+
+/etc/init.d/network reload
+sleep 10
+ip r del default dev eth2
+ip r add default via $ip dev eth2 metric 4
+ip r add 212.87.49.38 via $ip dev eth2
+#service network reload
+#/etc/init.d/network restart
 
 END=$(date '+%Y-%m-%d %H:%M:%S')
 DELTA=$(($(date -d "$END" +%s) - $(date -d "$START" +%s)))
 
-echo "---interface IP set, removing temp file" >> "$TEMP_FILE"
+echo "---interface IP and routes set, removing temp file" >> "$TEMP_FILE"
 echo "---$(date '+%Y-%m-%d %H:%M:%S'), time delta=$DELTA" >> "$TEMP_FILE"
 echo "-----------------------------------------------------------" >> "$TEMP_FILE"
-
+logger "Script fm350gl.sh ended"
 
 cat "$TEMP_FILE" >> "$LOG_FILE"
 rm "$TEMP_FILE"
