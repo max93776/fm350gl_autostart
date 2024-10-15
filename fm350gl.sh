c412e5120ff7f421de3d1957ae7b2f0942ee5309
max93776 <maxhdtube@gmail.com>
2024-10-15 16:22:52 +0200

    shell changes

--- fm350gl.sh
+++ fm350gl.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
-
+# Root dir
 cd
-
+# Paths
 TEMP_FILE=$(mktemp)
 LOG_FILE="/root/autostart/fm350gl_sh.log"
+# Start date for used time
 START=$(date '+%Y-%m-%d %H:%M:%S')
-
+# Logging
 logger "Script fm350gl.sh started"
 echo "-----------------------------------------------------------" >> "$TEMP_FILE"
 echo "---$(date '+%Y-%m-%d %H:%M:%S')" >> "$TEMP_FILE"
+# Activate generic driver
 echo "0e8d 7127" > /sys/bus/usb-serial/drivers/generic/new_id
 echo "---Installed driver, now waiting 10s" >> "$TEMP_FILE"
-sleep 10 
-
-for i in {1..10}; do
+# Modem has problems with power management turned on and high data rates, so diable power saving 
+echo -1 > /sys/module/usbcore/parameters/autosuspend 
+sleep 10
+# First loop for general connectivity
+for i in 1 2 3 4 5 6 7 8 9 10; do
 	timeout 5s /root/autostart/at_commander at >> "$TEMP_FILE"
 	if tail -n 1 "$TEMP_FILE" | grep -q "OK";then
 		echo "---AT command accepted" >> "$TEMP_FILE"
@@ -21,8 +25,8 @@ for i in {1..10}; do
 	fi
 	sleep 1  # Warte 1 Sekunde
 done
-
-for i in  {1..5}; do
+# Second loop for PDP context setting
+for i in 1 2 3 4; do
   timeout 5s /root/autostart/at_commander at+cgdcont=1,\"IP\",\"internet\" >> "$TEMP_FILE"
 	if tail -n 1 "$TEMP_FILE" | grep -q "OK";then
 		echo "---PDP context set" >> "$TEMP_FILE"
@@ -30,8 +34,8 @@ for i in  {1..5}; do
 	fi
 	sleep 1
 done 
-
-for i in {1..5}; do 
+# Third loop for PDP context activation
+for i in 1 2 3 4; do 
 	timeout 5s /root/autostart/at_commander at+cgact=1,1 >> "$TEMP_FILE"
 	if tail -n 1 "$TEMP_FILE" | grep -q "+CGEV";then
 		echo "---PDP activated" >> "$TEMP_FILE"
@@ -42,8 +46,8 @@ for i in {1..5}; do
 	fi
 	sleep 1
 done 
-
-for i in {1..5]; do
+# Fourth loop for getting IP address
+for i in 1 2 3 4; do
 	timeout 5s /root/autostart/at_commander at+cgpaddr=1 >> "$TEMP_FILE"
 	if tail -n 1 "$TEMP_FILE" | grep -q "+CGPADDR:";then
 		echo "---IP address sent" >> "$TEMP_FILE"
@@ -51,7 +55,7 @@ for i in {1..5]; do
 	fi
 	sleep 1
 done
-
+# Set IP address
 ip=$(tail -n 2 "$TEMP_FILE" | awk -F'"' '{print $2}')	 
 
 echo "---IP address: $ip" >> "$TEMP_FILE"
@@ -61,7 +65,7 @@ uci commit network
 /etc/init.d/network reload
 
 echo "---Setting routes for $ip" >> "$TEMP_FILE"
-
+# Setting routes
 /etc/init.d/network reload
 sleep 10
 ip r del default dev eth2
@@ -70,6 +74,7 @@ ip r add 212.87.49.38 via $ip dev eth2
 #service network reload
 #/etc/init.d/network restart
 
+# Calculate time delta and end script
 END=$(date '+%Y-%m-%d %H:%M:%S')
 DELTA=$(($(date -d "$END" +%s) - $(date -d "$START" +%s)))
 
